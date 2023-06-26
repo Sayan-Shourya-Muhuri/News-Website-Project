21af6a7efad812baee81a951fd26df8f8df60c1a
Sayan-Shourya-Muhuri <135993113+Sayan-Shourya-Muhuri@users.noreply.github.com>
2023-06-26 12:00:38 +0530

    Add files via upload

---
+++ JS1.js
@@ -0,0 +1,15 @@
+document.getElementById("demo").innerHTML="Hello World"
+console.log("Hello World")
+
+let x = myFunction(4,3)
+function myFunction(a,b){
+    return a*b;
+}
+console.log(x)
+
+const myFunction1 = (a,b) =>{
+    return a*b;
+}
+const y = myFunction1(3,6)
+console.log(y)
+
